c1c4a8efd8a9d35ebc7689713c3e9ddcc090a2f1
Shigatsi <shigatsii@gmail.com>
2021-11-19 13:22:32 +0300

    feat: project struct created

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "../Header/Header.js";
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+      <div className="content">CONTENT</div>
+      <footer className="footer">FOOTER</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <img
+        className="header__logo"
+        // src={#}
+        alt="Логотип ITGLOBAL"
+      />
+      HEADER
+    </header>
+  );
+}
+
+export default Header;
