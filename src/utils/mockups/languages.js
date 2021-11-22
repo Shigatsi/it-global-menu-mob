1591a45085a1a60ad947b905b06087832086d9aa
Shigatsi <shigatsii@gmail.com>
2021-11-22 23:34:18 +0300

    feat: mockups addded

---
+++ src/utils/mockups/languages.js
@@ -0,0 +1,47 @@
+import iconUs from '../../images/US.png'
+import iconNl from '../../images/Icon_NL.svg'
+import iconBy from '../../images/Icon_BY.svg'
+import iconRu from '../../images/Icon_RU.svg'
+import iconKz from '../../images/Icon_KZ.svg'
+import iconTr from '../../images/Icon_TR.svg'
+
+ const languages = [
+  {
+    country: 'United States',
+    icon: iconUs,
+    abbreviation: 'US'
+  },
+
+  {
+    country: 'Netherlands',
+    icon: iconNl,
+    abbreviation:'NL'
+  },
+
+  {
+    country: 'Беларусь',
+    icon: iconBy,
+    abbreviation: 'BY'
+  },
+
+  {
+    country: 'Россия',
+    icon: iconRu,
+    abbreviation: 'RU'
+  },
+
+  {
+    country: 'Казахстан',
+    icon: iconKz,
+    abbreviation: 'KZ'
+  },
+
+  {
+    country: 'Türkiye',
+    icon: iconTr,
+    abbreviation: 'TR'
+  },
+
+]
+
+export default languages;
