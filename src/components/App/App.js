6a7dd0df70c46558a403b46fb3dd7c73dcf5cca0
Shigatsi <shigatsii@gmail.com>
2021-11-22 19:42:13 +0300

    feat: footer added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "../Header/Header.js";
+import Footer from "../Footer/Footer.js";
 
 function App() {
   return (
     <div className="page">
       <Header />
       <div className="content">CONTENT</div>
-      <footer className="footer">FOOTER</footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+import "./Footer.css";
+
+function Footer() {
+  function getYear() {
+    return new Date().getFullYear();
+  }
+
+  const [width, setWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+  });
+
+  return (
+    <footer className="footer">
+      <p className="footer__subtitle">Тестовое задание ITGLOBAL.COM</p>
+      {width > 425 ? (
+        <>
+          <p className="footer__copyright">
+            &#169; Евгения "Shigatsi" Зубкова {getYear()}
+          </p>
+          <ul className="footer__nav">
+            <li className="foter__list-item">
+              <a
+                href="https://https://itglobal.com/ru-ru/"
+                className="footer__link"
+              >
+                ITGLOBAL.COM
+              </a>
+            </li>
+            <li className="foter__list-item">
+              <a href="https://github.com/Shigatsi" className="footer__link">
+                Github
+              </a>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <ul className="footer__nav">
+            <li className="foter__list-item">
+              <a
+                href="https://https://itglobal.com/ru-ru/"
+                className="footer__link"
+              >
+                ITGLOBAL.COM
+              </a>
+            </li>
+            <li className="foter__list-item">
+              <a href="https://github.com/Shigatsi" className="footer__link">
+                Github
+              </a>
+            </li>
+          </ul>
+          <p className="footer__copyright">
+            &#169;Евгения "Shigatsi" Зубкова {getYear()}{" "}
+          </p>
+        </>
+      )}
+    </footer>
+  );
+}
+
+export default Footer;
