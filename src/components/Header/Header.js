a9a07f299af2c5a83fa681962b4a3a336bff1e19
Shigatsi <shigatsii@gmail.com>
2021-11-23 15:16:42 +0300

    before merge

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,23 @@ import menuIcon from "../../images/icon_16x16_M_Hamburger.svg";
 import "./Header.css";
 
 function Header() {
+  const [menu, setMenu] = React.useState(false);
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
+
   return (
     <header className="header">
       <img className="header__logo" src={logoItGlobal} alt="Логотип ITGLOBAL" />
       <nav className="menu">
         <button className="menu__button" type="button">
-          <img className="menu__icon-menu" src={menuIcon} alt="иконка меню" />
+          <img
+            className="menu__icon-menu"
+            src={menuIcon}
+            onClick={toggleMenu}
+            alt="иконка меню"
+          />
         </button>
         {/* <ul className="menu__list">
           <li className="menu__item"></li>
