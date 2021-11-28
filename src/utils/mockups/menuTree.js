648ebc25e0ae3f3aee2bcc8ca820903e2db82171
Shigatsi <shigatsii@gmail.com>
2021-11-28 17:28:39 +0300

    feat: menuTree

---
+++ src/utils/mockups/menuTree.js
@@ -0,0 +1,140 @@
+const menuTree =[
+  {
+    item: 'Services',
+    subitems: [
+      {
+        title: "Облачные вычисления",
+        susubitems: [
+          {
+            title: "Cloud consulting",
+            about: "Relational database services for MySQL, PostgreSQL, and SQL server."
+          },
+          {
+            title: "Cloud infrastructure analytics",
+            about: "Health-specific solutions to enhance the patient experience."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Lorem ipsum dolor sit amet,",
+            about: "consectetur adipiscing elit. Aliquam tempor fermentum neque ultrices ornare."
+          }
+        ]
+      },
+      {
+        title: "Выделенные серверы",
+        susubitems: [
+          {
+            title: "Cloud consulting",
+            about: "Relational database services for MySQL, PostgreSQL, and SQL server."
+          },
+          {
+            title: "Cloud infrastructure analytics",
+            about: "Health-specific solutions to enhance the patient experience."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Lorem ipsum dolor sit amet,",
+            about: "consectetur adipiscing elit. Aliquam tempor fermentum neque ultrices ornare."
+          }
+        ]
+      },
+      {
+        title: "Платформенные сервисы",
+        susubitems: [
+          {
+            title: "Cloud consulting",
+            about: "Relational database services for MySQL, PostgreSQL, and SQL server."
+          },
+          {
+            title: "Cloud infrastructure analytics",
+            about: "Health-specific solutions to enhance the patient experience."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Lorem ipsum dolor sit amet,",
+            about: "consectetur adipiscing elit. Aliquam tempor fermentum neque ultrices ornare."
+          }
+        ]
+      },
+      {
+        title: "Информационная безопасность",
+        susubitems: [
+          {
+            title: "Cloud consulting",
+            about: "Relational database services for MySQL, PostgreSQL, and SQL server."
+          },
+          {
+            title: "Cloud infrastructure analytics",
+            about: "Health-specific solutions to enhance the patient experience."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Hybrid Cloud",
+            about: "Data storage, AI, and analytics solutions for government agencies."
+          },
+          {
+            title: "Lorem ipsum dolor sit amet,",
+            about: "consectetur adipiscing elit. Aliquam tempor fermentum neque ultrices ornare."
+          }
+        ]
+      },
+    ]
+  },
+
+  {
+    item: 'Managed IT',
+    subitems: [
+      {
+        title: ""
+      }
+    ]
+  },
+
+  {
+    item: 'Telecom Solutions',
+  },
+
+  {
+    item: 'About Us',
+  },
+]
